8f62aafe9084952b5a3bef81419eca694028bc70
Kenad Araujo <kenad@sefaz.pi.gov.br>
2021-11-24 13:37:44 -0300

    -Commit inicial

---
+++ src/main/java/com/polligonal/RedacaoEnem/RedacaoEnemApplication.java
@@ -0,0 +1,13 @@
+package com.polligonal.RedacaoEnem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedacaoEnemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedacaoEnemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polligonal/RedacaoEnem/RedacaoEnemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polligonal.RedacaoEnem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedacaoEnemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
